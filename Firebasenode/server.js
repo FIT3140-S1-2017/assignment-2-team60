99aa92bfeb17074ab948272b798227c763a56065
kscha39 <kscha39@student.monash.edu>
2017-04-01 16:49:09 +1100

    Update server.js

--- Firebasenode/server.js
+++ Firebasenode/server.js
@@ -2,7 +2,7 @@ var express        = require('express');
 var app            = express();  
 var httpServer = require("http").createServer(app);  
 var five = require("johnny-five");  
-var io = require('socket.io')(httpServer);
+// var io = require('socket.io')(httpServer);
 var admin = require("firebase-admin");
 
 // Fetch the service account key JSON file contents
@@ -11,172 +11,151 @@ var serviceAccount = require("./serviceAccountKey.json");
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://assignment2-500d8.firebaseio.com/"  // IMPORTANT: repalce the url with yours 
+  databaseURL: "https://project1-cb101.firebaseio.com/"  // IMPORTANT: repalce the url with yours 
 });
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = admin.database();
-var LongMotionData = db.ref("/Long Motion");
-LongMotionData.on("value", function(snapshot) {   //this callback will be invoked with each new object
-  console.log(snapshot.val());         // How to retrive the new added object
-}, function (errorObject) {             // if error
-  console.log("The read failed: " + errorObject.code);
-});
-var ShortMotionData = db.ref("/Short Motion");
-ShortMotionData.on("value", function(snapshot) {   
-  console.log(snapshot.val());         
-}, function (errorObject) {            
-  console.log("The read failed: " + errorObject.code);
-});
-var TotalMotionData = db.ref("/Total Motion");
-TotalMotionData.on("value", function(snapshot) {   
-  console.log(snapshot.val());         
-}, function (errorObject) {            
-  console.log("The read failed: " + errorObject.code);
-});
-
-// How to push new object
-app.get('/', function(req,res){
-
-		//send the index.html file for all requests
-		res.sendFile(__dirname + '/index.html');
-	});
-
-httpServer.listen(3000, function(){
-
-		console.log('listening on http://localhost:3000');
-	});
 
 //Connect to arduino
 var board = new five.Board();
 var led;
 var motion;
-board.on('ready', function(){ //to
-		console.log('Arduino connected');
-		led = new five.Led(13);
+board.on('ready', function(){
+	console.log('Arduino connected');
+	led = new five.Led(13);
 
-		motion = new five.Motion(2);
+	motion = new five.Motion(2);
+});
 
-	});
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
 
+var leddb = db.ref("/LED"); // channel name
+
+// if (leddb === null){
+// 	leddb.push({id:'LED', type:'OFF'});
+// }else{
+// 	leddb.update({id:'LED', type:'OFF'});
+// }
+var ledcounter = 0;
+leddb.on("value", function(snapshot) {   //this callback will be invoked with each new object
+	var dataled = snapshot.val();         // How to retrive the new added object
+	console.log(dataled);
+	if (ledcounter === 0){
+		ledcounter++
+		if (dataled !== 'null'){
+			leddb.update({id:'LED', type:'OFF'});
+		}else{
+			leddb.push({id:'LED', type:'OFF'});
+		}
+	}else{
+		var ledtype = snapshot.val().type;
+		if (ledtype === 'ON'){
+			led.on();
+		}else{
+			led.off();
+		}
+	}
+}, function (errorObject) {             // if error
+	console.log("The read failed: " + errorObject.code);
+});
+
+
+
+var total;
+var longmotion;
+var shortmotion;
+
+var sensordb = db.ref("/motionSensorData"); // channel name
+
+sensordb.on("value", function(snapshot) {   //this callback will be invoked with each new object
+	var datasensor = snapshot.val();         // How to retrive the new added object
+	console.log(datasensor);
+	total = snapshot.val().totaldata;
+	longmotion = snapshot.val().longdata;
+	shortmotion = snapshot.val().shortdata;
+	console.log("total: " + total);
+	console.log("longmotion: " + longmotion);
+	console.log("shortmotion: " + shortmotion);
+}, function (errorObject) {             // if error
+	console.log("The read failed: " + errorObject.code);
+});
+
+
+
+var bool = false;
 var time1;
 var time2;
 var diff;
-var total = 0;
-var longmotion = 0;
-var shortmotion = 0;
-var bool;
-var booldata= true;
-var IntruderArray =[];
-var Intruder=0;
-//socket connection handler
-io.on('connection', function(socket){
-		
-		socket.on('led:on',  function(data){
-			led.on();
-			console.log('Led is on!');
-		});
+var once = 0;
+var motiondb = db.ref("/Motion");
 
-		socket.on('led:off', function(data){
-			led.off();
-			console.log('Led is off!');
-		});
-
-		socket.on('motion:bool', function(data){
+var motioncounter = 0;
+motiondb.on("value", function(snapshot) {   //this callback will be invoked with each new object
+	datamotion = snapshot.val();
+	console.log(datamotion);         // How to retrive the new added object
+	if (motioncounter === 0){
+		motioncounter++
+		if (datamotion !== 'null'){
+			motiondb.update({id:'motion', type:'OFF'});
+		}else{
+			motiondb.push({id:'motion', type:'OFF'});
+		}
+	}else{
+		var motiontype = snapshot.val().type;
+		if (motiontype === 'ON'){
 			bool = true;
-		});
-
-		socket.on('sensor:on', function(data){
-			bool = true;
-			console.log('Motion On!');
-			motion.on("motionstart", function() {
-				if (booldata==false){
-					var LongMotionData = db.ref("/Long Motion");
-					LongMotionData.on("value", function(snapshot) {   //this callback will be invoked with each new object
-					snapshot.val();         // How to retrive the new added object
-					}, function (errorObject) {             // if error
-					console.log("The read failed: " + errorObject.code);
-					});
-					var ShortMotionData = db.ref("/Short Motion");
-					ShortMotionData.on("value", function(snapshot) {   
-					snapshot.val();         
-					}, function (errorObject) {             
-					console.log("The read failed: " + errorObject.code);
-					});
-					var TotalMotionData = db.ref("/Total Motion");
-					TotalMotionData.on("value", function(snapshot) {   
-					snapshot.val();         
-					}, function (errorObject) {    
-					console.log("The read failed: " + errorObject.code);
-					});
-					booldata=true;
-
-				}
+			if (once === 0){
+				motion.on("motionstart", function() {
+					if (bool === true){
+						console.log("Motion start");
+						time1 = new Date().getTime();
+					}
+				});
+			}else{
 				if (bool === true){
-					console.log("Motion Detected!");
+					console.log("Motion start");
 					time1 = new Date().getTime();
-					total++;
 				}
-			});
-
-			motion.on("motionend", function() {
+			}
+			if (once === 0){
+				once++;
+				motion.on("motionend", function() {
+					if (bool === true){
+						console.log("Motion end");
+						time2 = new Date().getTime();
+						diff = time2 - time1;
+						total = total + 1;
+						console.log(diff);
+						if (diff > 5000){
+							longmotion = longmotion + 1;
+						} else{
+							shortmotion= shortmotion +1;
+						};
+						sensordb.update({id:'data', totaldata:total, longdata:longmotion, shortdata:shortmotion});
+					}
+				});
+			}else{
 				if (bool === true){
-					console.log("Motion ended! ");
+					console.log("Motion end");
 					time2 = new Date().getTime();
 					diff = time2 - time1;
-					console.log("The time taken is " + diff/1000 + " seconds.");
-					console.log("The total motion detected is " + total + "\n");
-					socket.emit('total:motion', {motionno: total});
+					total = total + 1;
+					console.log(diff);
 					if (diff > 5000){
-						longmotion++;
-						IntruderArray.push("long");
-						TotalMotionData.push({id:'Long' ,type:'Motion' ,action:'Total Motion'});
-						LongMotionData.push({id:'Long' ,type:'Motion' ,action:'Long Motion',time:diff/1000});
+						longmotion = longmotion + 1;
 					} else{
-						shortmotion++;
-						IntruderArray.push("short");
-						TotalMotionData.push({id:'Short' ,type:'Motion' ,action:'Total Motion'});
-						ShortMotionData.push({id:'Short' ,type:'Motion' ,action:'Short Motion',time:diff/1000});
+						shortmotion= shortmotion +1;
 					};
-					socket.emit('long:motion', {motionlong: longmotion});
-					socket.emit('short:motion', {motionshort: shortmotion});
-					socket.emit('intruder:motion',{motionIntruder: Intruder });
-					if (IntruderArray.length==4){
-						if (IntruderArray==['long','short','long','long']){
-							Intruder++;
-							socket.emit('intruder:motion',{motionIntruder: Intruder });
-							IntruderArray=[];
-
-						}
-					}
-
+					sensordb.update({id:'data', totaldata:total, longdata:longmotion, shortdata:shortmotion});
 				}
-			});
-		});
-
-		socket.on('reset', function(data){
-			total = 0;
-			longmotion = 0;
-		 	shortmotion = 0;
-			Intruder=0;
-			IntruderArray=[]
-			socket.emit('total:motion', {motionno: total});
-			socket.emit('long:motion', {motionlong: longmotion});
-			socket.emit('short:motion', {motionshort: shortmotion});
-			socket.emit('intruder:motion',{motionIntruder: Intruder });
-			ShortMotionData.remove();
-			LongMotionData.remove();
-			TotalMotionData.remove();
-			booldata=false;
-
-		});
-
-		socket.on('sensor:off', function(data){
+			}
+		}else{
 			bool = false;
-			console.log('Motion Off')
-		});
+		}
+	}
+}, function (errorObject) {             // if error
+  console.log("The read failed: " + errorObject.code);
+});
 
-	});
 
 
 		
